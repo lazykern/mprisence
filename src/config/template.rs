4bb62ca7d7d08532557d79fd4a69d9a4d3f07188
Phusit Somboonyingsuk <phusitsom@gmail.com>
2023-07-02 11:12:01 +0700

    Support image text

--- src/config/template.rs
+++ src/config/template.rs
@@ -3,17 +3,26 @@ use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
 pub struct TemplateConfig {
-    #[serde(default = "default_detail")]
+    #[serde(default = "default_detail_template")]
     pub detail: String,
-    #[serde(default = "default_state")]
+    #[serde(default = "default_state_template")]
     pub state: String,
+    #[serde(default = "default_large_text_template")]
+    pub large_text: String,
+    #[serde(default = "default_small_text_template")]
+    pub small_text: String,
+    #[serde(default = "default_large_text_no_album_image_template")]
+    pub large_text_no_album_image: String,
 }
 
 impl Default for TemplateConfig {
     fn default() -> Self {
         Self {
-            detail: default_detail(),
-            state: default_state(),
+            detail: default_detail_template(),
+            state: default_state_template(),
+            large_text: default_large_text_template(),
+            small_text: default_small_text_template(),
+            large_text_no_album_image: default_large_text_no_album_image_template(),
         }
     }
 }
