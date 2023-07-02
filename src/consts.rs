49c09dd5eed05b9946c35e27f9ecca08694958c9
Phusit Somboonyingsuk <phusitsom@gmail.com>
2023-07-02 18:59:30 +0700

    Fix bad default template variables

--- src/consts.rs
+++ src/consts.rs
@@ -3,8 +3,8 @@ pub const DEFAULT_APP_ID: &str = "1121632048155742288";
 
 pub const DEFAULT_ICON: &str =
     "https://raw.githubusercontent.com/phusitsom/mprisence/main/assets/icon.png";
-pub const DEFAULT_DETAIL_TEMPLATE: &str = "{{{title}}} - {{{artist}}}";
-pub const DEFAULT_STATE_TEMPLATE: &str = "{{{album}}}";
+pub const DEFAULT_DETAIL_TEMPLATE: &str = "{{{title}}} - {{{artists}}}";
+pub const DEFAULT_STATE_TEMPLATE: &str = "{{{album_name}}}";
 pub const DEFAULT_LARGE_TEXT_TEMPLATE: &str = "{{{album_name}}}";
 pub const DEFAULT_SMALL_TEXT_TEMPLATE: &str = "{{{player}}}";
 pub const DEFAULT_LARGE_TEXT_NO_ALBUM_IMAGE_TEMPLATE: &str = "{{{album_name}}} - {{{player}}}";
