775fb8ffe58795095d5437db4249082bc6395164
Phusit Somboonyingsuk <phusitsom@gmail.com>
2023-07-03 18:18:30 +0700

    Set handlebars strict mode to false

--- src/hbs.rs
+++ src/hbs.rs
@@ -6,6 +6,7 @@ pub fn new_hbs() -> handlebars::Handlebars<'static> {
     let mut reg = handlebars_misc_helpers::new_hbs();
 
     reg.register_helper("eq", Box::new(eq));
+    reg.set_strict_mode(false);
 
     reg
 }
