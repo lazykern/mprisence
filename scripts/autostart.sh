321d56a5306cd0879c762929a0d2ac13778309a9
Phusit Somboonyingsuk <phusitsom@gmail.com>
2023-07-04 16:44:04 +0700

    Overwrite service file

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -6,34 +6,26 @@
 # 	by downloading it from https://raw.githubusercontent.com/phusitsom/mprisence/main/systemd/mprisence-local.service
 # 		and placing it in $HOME/.config/systemd/user/mprisence.service
 if [ -f "$HOME/.config/systemd/user/mprisence.service" ]; then
-	echo "mprisence.service exists"
-else
-	echo "mprisence.service does not exist"
-	echo "Create and start mprisence.service? (Y/n)"
-	read -r create_mprisence_service
-	if [ "$create_mprisence_service" != "n" ]; then
-
-		# Check for symlink at /usr/local/bin/mprisence
-		# If it exists, use it
-		# If it doesn't exist, create it
-		if [ -f "/usr/local/bin/mprisence" ]; then
-			echo "mprisence symlink exists"
+	# Check for symlink at /usr/local/bin/mprisence
+	# If it exists, use it
+	# If it doesn't exist, create it
+	if [ -f "/usr/local/bin/mprisence" ]; then
+		echo "mprisence symlink exists"
+	else
+		echo "mprisence symlink does not exist"
+		echo "Creating mprisence symlink"
+		if sudo ln -s "$(which mprisence)" /usr/local/bin/mprisence; then
+			echo "mprisence symlink created"
 		else
-			echo "mprisence symlink does not exist"
-			echo "Creating mprisence symlink"
-			if sudo ln -s "$(which mprisence)" /usr/local/bin/mprisence; then
-				echo "mprisence symlink created"
-			else
-				echo "mprisence symlink could not be created"
-				echo "Please create the symlink manually"
-				exit
-			fi
+			echo "mprisence symlink could not be created"
+			echo "Please create the symlink manually"
+			exit
 		fi
-
-		mkdir -p "$HOME/.config/systemd/user"
-		curl https://raw.githubusercontent.com/phusitsom/mprisence/main/systemd/mprisence-local.service >"$HOME/.config/systemd/user/mprisence.service"
-		systemctl --user daemon-reload
-		systemctl --user enable mprisence.service
-		systemctl --user start mprisence.service
 	fi
+
+	mkdir -p "$HOME/.config/systemd/user"
+	curl https://raw.githubusercontent.com/phusitsom/mprisence/main/systemd/mprisence-local.service >"$HOME/.config/systemd/user/mprisence.service"
+	systemctl --user daemon-reload
+	systemctl --user enable mprisence.service
+	systemctl --user start mprisence.service
 fi
