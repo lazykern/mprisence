f13435fe752fd931343cfdb11617f79bbbcb9edd
Phusit Somboonyingsuk <phusitsom@gmail.com>
2023-07-04 16:16:36 +0700

    Fix/scripts (#17)
    * Wrong helper url for autostart.sh
    
    * Redirect curl output instead of piping to sh

--- scripts/example-config.sh
+++ scripts/example-config.sh
@@ -1,6 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
-TEMP_DIR="$(mktemp -d)"
-curl https://raw.githubusercontent.com/phusitsom/mprisence/main/scripts/helper/example-config.sh >"$TEMP_DIR/example-config.sh"
-chmod +x "$TEMP_DIR/example-config.sh"
-"$TEMP_DIR/example-config.sh"
+CONFIG_URL=https://raw.githubusercontent.com/phusitsom/mprisence/main/config/example.toml
+CONFIG_PATH="${XDG_CONFIG_HOME:-$HOME/.config}/mprisence/config.toml"
+
+create_config() {
+	curl "$CONFIG_URL" >"$CONFIG_PATH"
+	echo "mprisence config created at $CONFIG_PATH"
+}
+
+if [ -f "$CONFIG_PATH" ]; then
+	echo "mprisence config exists"
+	echo "do you want to overwrite it? (y/N)"
+	echo "backup will be created at $CONFIG_PATH.bak"
+	read -r overwrite_config
+	if [ "$overwrite_config" = "y" ]; then
+		cp "$CONFIG_PATH" "$CONFIG_PATH.bak"
+		create_config
+	fi
+else
+	mkdir -p "$(dirname "$CONFIG_PATH")"
+	create_config
+fi
